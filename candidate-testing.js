7b5ab8cc203ee7202ca8d229c841cbea16c125a2
Kyle Feller <>
2021-05-22 21:37:15 +0000

    Completed all the part 1 challenges, the program now asks the user for their name, asks a question and checks the users answer against the correct answer. the resulting score is printed to the screen

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "Candidate's name" ;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = 'Answer this first question...\n';
+let correctAnswer = 'Correct answer';
+let candidateAnswer = "Candidate's Answer";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,12 +15,13 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+  candidateName = input.question('Enter your name... \n');
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+candidateAnswer = input.question(question);
 
 }
 
@@ -30,17 +31,22 @@ function gradeQuiz(candidateAnswers) {
 
 
   let grade;
-  
+  if(candidateAnswer===correctAnswer){
+    grade = 1;
+  } else{
+    grade = 0
+  }
 
-  return grade;
+  console.log(candidateName+' Your Grade is '+ grade*100 +'%');
 }
 
 function runProgram() {
-  askForName();
-  // TODO 1.1c: Ask for candidate's name //
   
+  // TODO 1.1c: Ask for candidate's name //
+  askForName();
   askQuestion();
   gradeQuiz(this.candidateAnswers);
+  
 }
 
 // Don't write any code below this line //
