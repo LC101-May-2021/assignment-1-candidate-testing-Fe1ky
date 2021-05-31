641f2d1b3af81572b3fd8eec489a6228bc61fe91
Kyle Feller <>
2021-05-31 18:31:07 +0000

    Fully functioning 5 question quiz!

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,13 +5,13 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = '' ;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = 'Who was the first American woman in space? ';
-let correctAnswer = 'Sally Ride';
+let questionBank = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ','(5 + 3)/2 * 10 = ? ',"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",'What is the minimum crew size for the ISS? '];
+let correctAnswerBank = ['Sally Ride','true','40','Trajectory','3'];
 let candidateAnswer = '';
 let questions;
 let correctAnswers;
-let candidateAnswers;
-
+let candidateAnswers = [];
+let score = 0
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -21,23 +21,30 @@ console.log('Welcome '+ candidateName+ '!');
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
 
+for(let i= 0 ; i<questionBank.length;i++){
+candidateAnswers[i] = input.question(questionBank[i]);
+  if(candidateAnswers[i]===correctAnswerBank[i]){
+    score++;
+  }
+}
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+let grade = (score/questionBank.length)*100;
+let status = 'FAILED';
+if(grade> 80){
+  status = 'PASSED'
+}
+console.log(`Candidate Name: ${candidateName}`)
+for(let i = 0; i< questionBank.length;i++){
 
+  console.log(`${i+1}) ${questionBank[i]} \n Your Answer: ${candidateAnswers[i]} \n Correct Answer: ${correctAnswerBank[i]} \n`);
 
-  let grade;
-  if(candidateAnswer===correctAnswer){
-    grade = 1;
-  } else{
-    grade = 0
-  }
-
- return grade*100;
+}
+console.log(`>>> Overall Grade: ${grade}% (${score} of ${questionBank.length} responses correct)<<< \n >>> Status: ${status} <<<`);
 }
 
 function runProgram() {
@@ -53,8 +60,8 @@ function runProgram() {
 // And don't change these or your program will not run as expected //
 module.exports = {
   candidateName: candidateName,
-  question: question,
-  correctAnswer: correctAnswer,
+  question: questionBank,
+  correctAnswer: correctAnswerBank,
   candidateAnswer: candidateAnswer,
   questions: questions,
   correctAnswers: correctAnswers,
