a107266648636ab1d96ea80bf4d162118e1bb660
Kyle Feller <>
2021-05-31 18:50:20 +0000

    changes to var names to try and get the autograder to like me

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,11 +5,11 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = '' ;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let questionBank = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ','(5 + 3)/2 * 10 = ? ',"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",'What is the minimum crew size for the ISS? '];
-let correctAnswerBank = ['Sally Ride','true','40','Trajectory','3'];
+let question ='Who was the first American woman in space?';
+let correctAnswer = 'Sally Ride';
 let candidateAnswer = '';
-let questions;
-let correctAnswers;
+let questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ','(5 + 3)/2 * 10 = ? ',"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",'What is the minimum crew size for the ISS? '];;
+let correctAnswers =['Sally Ride','true','40','Trajectory','3'];
 let candidateAnswers = [];
 let score = 0
 
@@ -22,9 +22,9 @@ console.log('Welcome '+ candidateName+ '!');
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-for(let i= 0 ; i<questionBank.length;i++){
-candidateAnswers[i] = input.question(questionBank[i]);
-  if(candidateAnswers[i]==correctAnswerBank[i]){
+for(let i= 0 ; i<questions.length;i++){
+candidateAnswers[i] = input.question(questions[i]);
+  if(candidateAnswers[i]===correctAnswers[i]){
     score++;
   }
 }
@@ -33,18 +33,18 @@ candidateAnswers[i] = input.question(questionBank[i]);
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-let grade = (score/questionBank.length)*100;
+let grade = (score/questions.length)*100;
 let status = 'FAILED';
 if(grade> 80){
   status = 'PASSED'
 }
 console.log(`Candidate Name: ${candidateName}`)
-for(let i = 0; i< questionBank.length;i++){
+for(let i = 0; i< questions.length;i++){
 
-  console.log(`${i+1}) ${questionBank[i]} \n Your Answer: ${candidateAnswers[i]} \n Correct Answer: ${correctAnswerBank[i]} \n`);
+  console.log(`${i+1}) ${questions[i]} \n Your Answer: ${candidateAnswers[i]} \n Correct Answer: ${correctAnswers[i]} \n`);
 
 }
-console.log(`>>> Overall Grade: ${grade}% (${score} of ${questionBank.length} responses correct)<<< \n >>> Status: ${status} <<<`);
+console.log(`>>> Overall Grade: ${grade}% (${score} of ${questions.length} responses correct)<<< \n >>> Status: ${status} <<<`);
 }
 
 function runProgram() {
@@ -60,8 +60,8 @@ function runProgram() {
 // And don't change these or your program will not run as expected //
 module.exports = {
   candidateName: candidateName,
-  question: questionBank,
-  correctAnswer: correctAnswerBank,
+  question: question,
+  correctAnswer: correctAnswer,
   candidateAnswer: candidateAnswer,
   questions: questions,
   correctAnswers: correctAnswers,
