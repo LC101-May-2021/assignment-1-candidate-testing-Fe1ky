5b76b2d4deca4347b04818a39948024a22983a02
Kyle Feller <>
2021-06-06 19:11:13 +0000

    moved the score Variable inside the gradeQuiz function

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,7 +11,7 @@ let candidateAnswer = '';
 let questions = ['Who was the first American woman in space? ', 'True or false: 5 kilometer == 5000 meters? ','(5 + 3)/2 * 10 = ? ',"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",'What is the minimum crew size for the ISS? '];
 let correctAnswers =['Sally Ride','true','40','Trajectory','3'];
 let candidateAnswers = [];
-let score = 0;
+
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -30,7 +30,7 @@ for(let i= 0 ; i<questions.length;i++){
 
 
 function gradeQuiz(candidateAnswers) {
-
+let score = 0;
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 for(let i=0;i<questions.length;i++){
   if(candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
@@ -52,7 +52,7 @@ for(let i = 0; i< questions.length;i++){
 }
 console.log(`>>> Overall Grade: ${grade}% (${score} of ${questions.length} responses correct)<<< \n >>> Status: ${status} <<<`);
 return grade
-score = 0 
+
 }
 
 function runProgram() {
